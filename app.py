e6c7180f0068648337f6ff0acc9dd34619ad9617
Julien Lebunetel <julien@lebunetel.com>
2019-02-06 11:09:33 +0100

    error handler for 404

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
