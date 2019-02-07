b6fa299017aa127d5b84b49c6a42091f8b0694a7
Julien Lebunetel <julien@lebunetel.com>
2019-02-07 10:07:33 +0100

    error message, bom, github file loading process

--- app.py
+++ app.py
@@ -3,8 +3,52 @@ import requests
 import json
 from jsonschema import validate
 from urllib.parse import urlparse
+from uuid import uuid4
 
 app = Flask(__name__)
+
+
+def load_github_file(organisation, repository, file_name):
+    # download file
+    base_url = "https://raw.githubusercontent.com/{0}/{1}/master/".format(organisation, repository)
+    full_file_name = file_name + '.json'
+    url = base_url + full_file_name
+    r = requests.get(url)
+    if r.status_code != 200:
+        return {}, {'error': 'File not found.'}
+
+    # parse json
+    try:
+        data = r.json()
+    except:
+        return {}, {'error': 'Invalid JSON file'}
+
+    with open(full_file_name, "r") as file:
+        json_schema = json.load(file)
+
+    # validate the source according to JSON SCHEMA
+    try:
+        validate(data, json_schema)
+    except Exception as valid_err:
+        return {}, {'error': 'Invalid JSON file according to JSON SCHEMA', 'message': valid_err}
+
+    data['base_url'] = base_url
+    return data, 0
+
+
+def component_complete(component, components_library):
+    """Try to complete components caracteristics from components library"""
+    if 'id' in component:
+        for reference_component in components_library['components']:
+            if component['id'] == reference_component['id']:
+                return dict(reference_component, **component) # remplace les clés présentes dans reference_component par celles de component si elles existent
+    default_component = {
+        'name': 'composant inconnu',
+        'id': str(uuid4()),
+        'quantity': 1
+    }
+    return dict(default_component, **component)
+
 
 @app.context_processor
 def utility_processor():
@@ -17,55 +61,62 @@ def utility_processor():
             return url
     return dict(format_url=format_url)
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
-    #return render_template('404.html'), 404
-    return render_template('error.html', title='404 - Not found', details='404 - Not found'), 404
+    return render_template('error.html', title='404 - Not found', error={'error': '404 - Not found'}), 404
+
 
 @app.route('/<string:organisation>/<string:repository>')
 def project_details(organisation, repository):
+    # load project datas
+    project, error = load_github_file(organisation, repository, 'project')
+    if error:
+        return render_template('error.html', title='400 - Bad Request', error=error), 400
 
-    base_url = "https://raw.githubusercontent.com/{0}/{1}/master/".format(organisation, repository)
-
-    # download project description
-    url = base_url + "project.json"
-    r = requests.get(url)
-    if r.status_code != 200:
-        return render_template('error.html', title='400 - Bad Request', details='Project not found.'), 400
-
-    # parse json
-    try:
-        data = r.json()
-    except:
-        return render_template('error.html', title='400 - Bad Request', details='The source file is not a valid JSON.'), 400
-
-    with open("./project.json", "r") as file:
-        json_schema = json.load(file)
-
-    # validate the source according to JSON SCHEMA
-    try:
-        validate(data, json_schema)
-    except Exception as valid_err:
-        return render_template('error.html', title='400 - Bad Request', details='The source file is not a valid JSON.', message=valid_err), 400
+    # load local libraries
+    components_library, error = load_github_file(organisation, repository, 'components')
+    if error:
+        components_library = {"components": []}
 
     # some datas are computed
     duration = 0
+    components = []
 
-    if 'steps' in data['project']:
-        for step in data['project']['steps']:
+    if 'steps' in project['project']:
+        step_index = 0
+        for step in project['project']['steps']:
+            # calculation of cumulative duration
             if 'duration' in step:
                 duration += step['duration']
 
-    data['project']['computed'] = {
-        "duration": duration
+            # on complete les informations sur les composants par ceux issus de la librairie
+            if 'components' in step:
+                component_index = 0
+                for component in step['components']:
+                    project['project']['steps'][step_index]['components'][component_index] = component_complete(component, components_library)
+                    component_index += 1
+
+            step_index += 1
+
+        # calculation of the B.O.M.
+        for step in project['project']['steps']:
+            if 'components' in step:
+                for component in step['components']:
+                    components.append(component)
+
+    # add computed datas to project datas
+    project['project']['computed'] = {
+        "duration": duration,
+        "components": components
     }
 
-    data['project']['base_url'] = base_url
+    project['project']['base_url'] = project['base_url']
 
     # project rendering
-    return render_template('project.html', project=data['project'])
+    return render_template('project.html', project=project['project'])
